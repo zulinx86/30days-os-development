9385a54d8d500ce3331294d07fc3a035a783a8d7
zulinx86 <zulinx86@gmail.com>
2021-07-18 23:24:15 +0900

    fix a comment about video mode

--- day03/harib00g/haribote.asm
+++ day03/harib00g/haribote.asm
@@ -3,7 +3,7 @@
 
 	ORG	0xc200
 
-	MOV	AL,0x13		; VGA graphics (640 x 480 x 8 bit color)
+	MOV	AL,0x13		; VGA graphics (320 x 200 x 8 bit color)
 	MOV	AH,0x00		; function to set video mode
 	INT	0x10		; BIOS interruption for video display functions
 fin:
