5179e9f75ce2833ced749ddf7094ba29a92543c7
zulinx86 <zulinx86@gmail.com>
2021-07-19 21:08:30 +0900

    add day09/harib06b

---
+++ day09/harib06b/bootpack.c
@@ -0,0 +1,133 @@
+#include "mystdio.h"
+#include "bootpack.h"
+
+unsigned int memtest(unsigned int start, unsigned int end);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+
+void HariMain(void)
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	char s[40], mcursor[256], keybuf[32], mousebuf[128];
+	int mx, my, i;
+	struct MOUSE_DEC mdec;
+
+	init_gdtidt();
+	init_pic();
+	io_sti();
+
+	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
+	io_out8(PIC0_IMR, 0xf9); /* enable PIC1 and PS/2 keyboard (11111001) */
+	io_out8(PIC1_IMR, 0xef); /* enable PS/2 mouse (11101111) */
+
+	init_keyboard();
+	enable_mouse(&mdec);
+
+	init_palette();
+	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2;
+	my = (binfo->scrny - 28 - 16) / 2;
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	mysprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
+	mysprintf(s, "memory %dMB", i);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+
+	for (;;) {
+		io_cli();
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
+			io_stihlt();
+		} else {
+			if (fifo8_status(&keyfifo) != 0) {
+				i = fifo8_get(&keyfifo);
+				io_sti();
+				mysprintf(s, "%02X", i);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			} else if (fifo8_status(&mousefifo) != 0) {
+				i = fifo8_get(&mousefifo);
+				io_sti();
+				if (mouse_decode(&mdec, i) != 0) {
+					mysprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) s[1] = 'L';
+					if ((mdec.btn & 0x02) != 0) s[3] = 'R';
+					if ((mdec.btn & 0x04) != 0) s[2] = 'C';
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* erase mouse */
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) mx = 0;
+					if (my < 0) my = 0;
+					if (mx > binfo->scrnx - 16) mx = binfo->scrnx - 16;
+					if (my > binfo->scrny - 16) my = binfo->scrny - 16;
+					mysprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* erase coordinates */
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* paint coordinates */
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* paint mouse */
+				}
+			}
+		}
+	}
+}
+
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	/* check whether the CPU is 386, or 486 and later */
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) /* if the CPU is 386, AC is back to 0 automatically */
+		flg486 = 1;
+	eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE; /* disable CPU cache */
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+unsigned int memtest_sub(unsigned int start, unsigned int end)
+{
+	unsigned int i, *p, old, pat0 = 0xaa55aa55, pat1 = 0x55aa55aa;
+	for (i = start; i <= end; i += 0x1000) {
+		p = (unsigned int *) (i + 0xffc);
+		old = *p;			/* old value */
+		*p = pat0;			/* write pattern 0 */
+		*p ^= 0xffffffff;	/* inverse */
+		if (*p != pat1) {	/* compare with pattern 1 */
+not_memory:
+			*p = old;
+			break;
+		}
+		*p ^= 0xffffffff;	/* inverse again */
+		if (*p != pat0) {	/* compare with pattern 0 */
+			goto not_memory;
+		}
+		*p = old;			/* set old value back */
+	}
+	return i;
+}
