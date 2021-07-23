8c58c95d1d83403dd5e0b721cacf755819ac053d
zulinx86 <zulinx86@gmail.com>
2021-07-23 22:44:26 +0900

    add day15/harib12d

---
+++ day15/harib12d/memory.c
@@ -0,0 +1,139 @@
+#include "bootpack.h"
+
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	/* check whether the CPU is 386, or 486 and later */
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) /* if the CPU is 386, AC is back to 0 automatically */
+		flg486 = 1;
+	eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE; /* disable CPU cache */
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+void memman_init(struct MEMMAN *man)
+{
+	man->frees = 0;		/* number of free spaces */
+	man->maxfrees = 0;	/* max value of frees for observing memory fragmentation */
+	man->lostsize = 0;	/* sum of size of lost spaces */
+	man->losts = 0;		/* number of lost spaces */
+	return;
+}
+
+unsigned int memman_total(struct MEMMAN *man)
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++)
+		t += man->free[i].size;
+	return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				man->frees--;
+				for (; i < man->frees; i++)
+					man->free[i] = man->free[i + 1];
+			}
+			return a;
+		}
+	}
+	return 0;
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+	int i, j;
+
+	/* free[i - 1].addr < addr < free[i].addr */
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) break;
+	}
+
+	/* if it's not the head */
+	if (i > 0) {
+		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+			man->free[i - 1].size += size;
+			if (addr + size == man->free[i].addr) {
+				man->free[i - 1].size += man->free[i].size;
+				man->frees--;
+				for (; i < man->frees; i++)
+					man->free[i] = man->free[i + 1];
+			}
+			return 0;
+		}
+	}
+
+	/* cannot merge it with the front */
+	if (i < man->frees) {
+		if (addr + size == man->free[i].addr) {
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0;
+		}
+	}
+
+	/* cannot merge it with both the front and the back */
+	if (man->frees < MEMMAN_FREES) {
+		for (j = man->frees; j > i; j--)
+			man->free[j] = man->free[j - 1];
+
+		man->frees++;
+		if (man->maxfrees < man->frees)
+			man->maxfrees = man->frees;
+
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0;
+	}
+
+	man->losts++;
+	man->lostsize += size;
+	return -1;
+}
+
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int a;
+	size = (size + 0xfff) & 0xfffff000;
+	a = memman_alloc(man, size);
+	return a;
+}
+
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+	int i;
+	size = (size + 0xfff) & 0xfffff000;
+	i = memman_free(man, addr, size);
+	return i;
+}
