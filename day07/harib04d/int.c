d731af9fd0c89b4600488dead026f2abe95d3ecd
zulinx86 <zulinx86@gmail.com>
2021-07-23 13:51:25 +0900

    remove an unused variable

--- day07/harib04d/int.c
+++ day07/harib04d/int.c
@@ -27,7 +27,7 @@ struct KEYBUF keybuf;
 void inthandler21(int *esp)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	unsigned char data, s[4];
+	unsigned char data;
 	io_out8(PIC0_OCW2, 0x61); /* notify PIC of completion of IRQ-01 reception */
 	data = io_in8(PORT_KEYDAT);
 	if (keybuf.len < 32) {
