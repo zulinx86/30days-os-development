290c0d51e8a835a62fd7d8e55cb0f4cd64e86cbf
zulinx86 <zulinx86@gmail.com>
2021-07-26 17:53:34 +0900

    change the characters to display from 'A' to 'hello'

--- day20/harib17d/hlt.asm
+++ day20/harib17d/hlt.asm
@@ -1,5 +1,13 @@
 [BITS 32]
 
-	MOV		AL,'A'
+	MOV		AL,'h'
+	CALL	2*8:0x15a
+	MOV		AL,'e'
+	CALL	2*8:0x15a
+	MOV		AL,'l'
+	CALL	2*8:0x15a
+	MOV		AL,'l'
+	CALL	2*8:0x15a
+	MOV		AL,'o'
 	CALL	2*8:0x15a
 	RETF
