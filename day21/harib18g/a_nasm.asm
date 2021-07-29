05d9b73ba8601d4d8b9c4904df1dec174aa3028a
zulinx86 <zulinx86@gmail.com>
2021-07-29 15:37:19 +0900

    add day21/harib18g

---
+++ day21/harib18g/a_nasm.asm
@@ -0,0 +1,16 @@
+[BITS 32]
+
+		GLOBAL	api_putchar
+		GLOBAL	api_end
+
+[SECTION .text]
+
+api_putchar:	; void api_putchar(int c);
+		MOV		EDX,1
+		MOV		AL,[ESP+4]
+		INT		0x40
+		RET
+
+api_end:		; void api_end(void);
+		MOV		EDX,4
+		INT		0x40
