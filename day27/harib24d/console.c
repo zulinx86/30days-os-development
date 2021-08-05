fe3a50d73b1d53e96f4045f9aebb895ef9befff4
zulinx86 <zulinx86@gmail.com>
2021-08-06 08:52:00 +0900

    remove an unused variable

--- day27/harib24d/console.c
+++ day27/harib24d/console.c
@@ -306,7 +306,6 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 {
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct FILEINFO *finfo;
-	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
 	char name[18], *p, *q;
 	struct TASK *task = task_now();
 	int i, segsize, datasize, esp, datahrb;
